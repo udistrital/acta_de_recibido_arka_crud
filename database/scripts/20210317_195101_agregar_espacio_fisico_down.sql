9f7cac9758868361e42db8447482491697c2dfae
ludwinggsanchez <ludwingsanchezleon@gmail.com>
2021-03-17 22:41:13 -0500

    feat: Insert en tabla paramétrica tipo_bien

--- database/scripts/20210317_195101_agregar_espacio_fisico_down.sql
+++ database/scripts/20210317_195101_agregar_espacio_fisico_down.sql
@@ -1,1 +1,4 @@
 ALTER TABLE acta_recibido.elemento DROP column espacio_fisico_id INTEGER
+DELETE FROM acta_recibido.tipo_bien
+    WHERE id = 4;
+COMMENT ON COLUMN acta_recibido.elemento.espacio_fisico_id IS '';
--- database/scripts/20210317_195101_agregar_espacio_fisico_up.sql
+++ database/scripts/20210317_195101_agregar_espacio_fisico_up.sql
@@ -1,1 +1,4 @@
-ALTER TABLE acta_recibido.elemento ADD column espacio_fisico_id INTEGER
+ALTER TABLE acta_recibido.elemento ADD column espacio_fisico_id INTEGER;
+INSERT INTO acta_recibido.tipo_bien (id, nombre, descripcion, codigo_abreviacion, activo, numero_orden, fecha_creacion, fecha_modificacion) 
+	VALUES (4, 'Inmueble', 'Bien de tipo inmueble', 'Inmueble', true, 4, now(), now());
+COMMENT ON COLUMN acta_recibido.elemento.espacio_fisico_id IS 'Hace referencia al id en la tabla espacio_físico en el api oikos_crud, en caso de que el elemento sea de tipo bien inmueble';
