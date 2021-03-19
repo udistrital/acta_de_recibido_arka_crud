206b124780811a102942f83212e934a3223e5f36
ludwinggsanchez <ludwingsanchezleon@gmail.com>
2021-03-19 12:14:07 -0500

    fix: Down script modified

--- database/scripts/20210317_195101_agregar_espacio_fisico_down.sql
+++ database/scripts/20210317_195101_agregar_espacio_fisico_down.sql
@@ -1,4 +1,3 @@
-ALTER TABLE acta_recibido.elemento DROP column espacio_fisico_id INTEGER
+ALTER TABLE acta_recibido.elemento DROP column espacio_fisico_id;
 DELETE FROM acta_recibido.tipo_bien
     WHERE id = 4;
-COMMENT ON COLUMN acta_recibido.elemento.espacio_fisico_id IS '';
