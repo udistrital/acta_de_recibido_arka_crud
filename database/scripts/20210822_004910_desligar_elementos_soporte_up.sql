ad5d9909058c7a8fcce1ec06d377e6e3ea8fe412
ludwinggsanchez <ludwingsanchezleon@gmail.com>
2021-09-04 03:27:54 -0500

    docs: v0.1.0: Revertir rollback de espacio_fisico_id

--- database/scripts/20210822_004910_desligar_elementos_soporte_up.sql
+++ database/scripts/20210822_004910_desligar_elementos_soporte_up.sql
@@ -3,8 +3,8 @@
 ALTER TABLE acta_recibido.elemento
     ADD COLUMN IF NOT EXISTS acta_recibido_id INTEGER;
 
--- UPDATE acta_recibido.elemento
---     SET acta_recibido_id = COALESCE(soporte_acta_id);
+UPDATE acta_recibido.elemento
+    SET acta_recibido_id = COALESCE(soporte_acta_id);
 --
 ALTER TABLE acta_recibido.elemento
     DROP COLUMN IF EXISTS verificado;
@@ -12,13 +12,13 @@ ALTER TABLE acta_recibido.elemento
 ALTER TABLE acta_recibido.elemento
     DROP COLUMN IF EXISTS soporte_acta_id;
 --
--- ALTER TABLE acta_recibido.elemento
---     ALTER COLUMN acta_recibido_id SET NOT NULL;
---
--- ALTER TABLE acta_recibido.elemento
---     ADD CONSTRAINT fk_elemento_acta_recibido FOREIGN KEY (acta_recibido_id)
---     REFERENCES acta_recibido.acta_recibido (id) MATCH FULL
---     ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE acta_recibido.elemento
+    ALTER COLUMN acta_recibido_id SET NOT NULL;
+
+ALTER TABLE acta_recibido.elemento
+    ADD CONSTRAINT fk_elemento_acta_recibido FOREIGN KEY (acta_recibido_id)
+    REFERENCES acta_recibido.acta_recibido (id) MATCH FULL
+    ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- Historico
 --
