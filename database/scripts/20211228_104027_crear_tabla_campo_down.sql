1af292efe8aaf3ad290b7b3029e0c979b958ef79
MateoAlfonso <bmrodrigueza@correo.udistrital.edu.co>
2022-01-01 13:03:26 -0500

    feat:Pólizas

--- database/scripts/20211228_104027_crear_tabla_campo_down.sql
+++ database/scripts/20211228_104027_crear_tabla_campo_down.sql
@@ -1,11 +1,19 @@
+-- [ Dropped objects ] --
 ALTER TABLE acta_recibido.elemento_campo DROP CONSTRAINT IF EXISTS fk_elemento_campo_campo CASCADE;
-
+-- ddl-end --
 ALTER TABLE acta_recibido.elemento_campo DROP CONSTRAINT IF EXISTS fk_elemento_campo_elemento CASCADE;
-
+-- ddl-end --
 DROP TABLE IF EXISTS acta_recibido.campo CASCADE;
-
+-- ddl-end --
 DROP SEQUENCE IF EXISTS acta_recibido.campo_id_seq CASCADE;
-
+-- ddl-end --
 DROP TABLE IF EXISTS acta_recibido.elemento_campo CASCADE;
-
+-- ddl-end --
 DROP SEQUENCE IF EXISTS acta_recibido.elemento_campo_id_seq CASCADE;
+-- ddl-end --
+DROP TABLE IF EXISTS public.migrations CASCADE;
+-- ddl-end --
+DROP SEQUENCE IF EXISTS public.migrations_id_migration_seq CASCADE;
+-- ddl-end --
+DROP TYPE IF EXISTS public.migrations_status CASCADE;
+-- ddl-end --
