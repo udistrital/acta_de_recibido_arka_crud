0ee0a050ca37ac608e128a923ed6a2ba1025186c
MateoAlfonso <bmrodrigueza@correo.udistrital.edu.co>
2022-01-02 11:46:52 -0500

    feat:router controlador campo udistrital/arka_cliente#875

--- routers/commentsRouter_controllers.go
+++ routers/commentsRouter_controllers.go
@@ -349,4 +349,48 @@ func init() {
 			Filters:          nil,
 			Params:           nil})
 
+	beego.GlobalControllerRouter["github.com/udistrital/acta_recibido_crud/controllers:CampoController"] = append(beego.GlobalControllerRouter["github.com/udistrital/acta_recibido_crud/controllers:CampoController"],
+		beego.ControllerComments{
+			Method:           "Post",
+			Router:           "/",
+			AllowHTTPMethods: []string{"post"},
+			MethodParams:     param.Make(),
+			Filters:          nil,
+			Params:           nil})
+
+	beego.GlobalControllerRouter["github.com/udistrital/acta_recibido_crud/controllers:CampoController"] = append(beego.GlobalControllerRouter["github.com/udistrital/acta_recibido_crud/controllers:CampoController"],
+		beego.ControllerComments{
+			Method:           "GetAll",
+			Router:           "/",
+			AllowHTTPMethods: []string{"get"},
+			MethodParams:     param.Make(),
+			Filters:          nil,
+			Params:           nil})
+
+	beego.GlobalControllerRouter["github.com/udistrital/acta_recibido_crud/controllers:CampoController"] = append(beego.GlobalControllerRouter["github.com/udistrital/acta_recibido_crud/controllers:CampoController"],
+		beego.ControllerComments{
+			Method:           "GetOne",
+			Router:           "/:id",
+			AllowHTTPMethods: []string{"get"},
+			MethodParams:     param.Make(),
+			Filters:          nil,
+			Params:           nil})
+
+	beego.GlobalControllerRouter["github.com/udistrital/acta_recibido_crud/controllers:CampoController"] = append(beego.GlobalControllerRouter["github.com/udistrital/acta_recibido_crud/controllers:CampoController"],
+		beego.ControllerComments{
+			Method:           "Put",
+			Router:           "/:id",
+			AllowHTTPMethods: []string{"put"},
+			MethodParams:     param.Make(),
+			Filters:          nil,
+			Params:           nil})
+
+	beego.GlobalControllerRouter["github.com/udistrital/acta_recibido_crud/controllers:CampoController"] = append(beego.GlobalControllerRouter["github.com/udistrital/acta_recibido_crud/controllers:CampoController"],
+		beego.ControllerComments{
+			Method:           "Delete",
+			Router:           "/:id",
+			AllowHTTPMethods: []string{"delete"},
+			MethodParams:     param.Make(),
+			Filters:          nil,
+			Params:           nil})
 }
