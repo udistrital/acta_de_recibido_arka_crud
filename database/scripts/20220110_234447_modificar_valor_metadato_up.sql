676204cc28218eb0506158f0a5e9244df8cde788
Mateo Rodriguez Alfonso <93339666+MateoAlfons@users.noreply.github.com>
2022-02-05 21:31:22 -0500

    Correciones sugeridas aplicadas
    udistrital/arka_cliente#875
    
    Co-authored-by: Alex Bustos <AlexFBP@users.noreply.github.com>

--- database/scripts/20220110_234447_modificar_valor_metadato_up.sql
+++ database/scripts/20220110_234447_modificar_valor_metadato_up.sql
@@ -1,8 +1,12 @@
 ALTER TABLE acta_recibido.campo
     ADD COLUMN metadato jsonb;
 
+-- ADVERTENCIA: Las 2 sentencias SQL siguientes implican
+-- una POSIBLE PÉRDIDA DE DATOS.
+-- Se recomienda mejorar las dos líneas siguientes para que en
+-- vez de simplemente eliminar y volver a agregar la columna,
+-- se transformen los valores que estaban antes
 ALTER TABLE acta_recibido.elemento_campo
     DROP COLUMN IF EXISTS valor CASCADE;
-
 ALTER TABLE acta_recibido.elemento_campo
     ADD COLUMN valor jsonb;
