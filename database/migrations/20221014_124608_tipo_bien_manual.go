c1d8e16b779c87992f3253fdc5aa2b6eada37a3d
Ludwing Sanchez <ludwingsanchezleon@gmail.com>
2022-10-14 13:24:24 -0500

    feat udistrital/arka_mid#254, udistrital/arka_cliente#978: Umbrales
    - Se hace el ajuste en el modelo para asignar el tipo de bien de manera manual si se desea.

---
+++ database/migrations/20221014_124608_tipo_bien_manual.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"strings"
+
+	"github.com/astaxie/beego/migration"
+)
+
+// DO NOT MODIFY
+type TipoBienManual_20221014_124608 struct {
+	migration.Migration
+}
+
+// DO NOT MODIFY
+func init() {
+	m := &TipoBienManual_20221014_124608{}
+	m.Created = "20221014_124608"
+
+	migration.Register("TipoBienManual_20221014_124608", m)
+}
+
+// Run the migrations
+func (m *TipoBienManual_20221014_124608) Up() {
+	file, err := ioutil.ReadFile("../scripts/20221014_124608_tipo_bien_manual_up.sql")
+
+	if err != nil {
+		// handle error
+		fmt.Println(err)
+	}
+
+	requests := strings.Split(string(file), ";")
+
+	for _, request := range requests {
+		fmt.Println(request)
+		m.SQL(request)
+	}
+
+}
+
+// Reverse the migrations
+func (m *TipoBienManual_20221014_124608) Down() {
+	file, err := ioutil.ReadFile("../scripts/20221014_124608_tipo_bien_manual_down.sql")
+
+	if err != nil {
+		// handle error
+		fmt.Println(err)
+	}
+
+	requests := strings.Split(string(file), ";")
+
+	for _, request := range requests {
+		fmt.Println(request)
+		m.SQL(request)
+	}
+
+}
